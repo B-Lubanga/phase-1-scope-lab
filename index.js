a14e5537dfee43e8d99e568061a107ae7a437f5d
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-03-20 19:53:56 +0300

    done done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = "not bob"; // Creates a global variable
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "some customer";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "another customer"; // This will throw an error
+}
